0a44a2c3f1583a557c36e0f115c95be117bcea59
suhs <suh.sai@citrix.com>
2017-10-13 17:26:56 -0400

    first commit

---
+++ AWSSESSample/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AWSSESSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(407, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(210, 135);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(407, 311);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 547);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 92);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 547);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 92);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Query SQS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "success@simulator.amazonses.com",
+            "bounce@simulator.amazonses.com",
+            "ooto@simulator.amazonses.com",
+            "complaint@simulator.amazonses.com",
+            "suppressionlist@simulator.amazonses.com"});
+            this.comboBox1.Location = new System.Drawing.Point(210, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(407, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 741);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AWSSESSample/Form1.cs
@@ -0,0 +1,88 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace AWSSESSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = "test1";
+            textBox3.Text = "test body 111";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            SmtpClient client = new SmtpClient(appSettings["SMTPHost"], Convert.ToInt32(appSettings["SMTPPort"]));
+            client.Credentials = new NetworkCredential(appSettings["SMTPUser"], appSettings["SMTPPW"]);
+            client.EnableSsl = true;
+
+            MailMessage message = new MailMessage("saisuu@hotmail.com", comboBox1.Text);
+            message.Subject = textBox2.Text;
+            message.Body = textBox3.Text;
+
+            client.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
+            client.SendAsync(message, "test-suh");
+            //message.Dispose();
+        }
+
+        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
+
+            if (e.Cancelled)
+            {
+                MessageBox.Show(string.Format("[{0}] Send canceled.", token));
+            }
+            if (e.Error != null)
+            {
+                MessageBox.Show(string.Format("[{0}] {1}", token, e.Error.ToString()));
+            }
+            else
+            {
+                MessageBox.Show("Message sent.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            AmazonSQSClient sqsClient = new AmazonSQSClient(appSettings["AWSKeyID"], appSettings["AWSAccessKey"], RegionEndpoint.USEast1);
+            var queueUrl = sqsClient.ListQueues("Queue-For-SES-Notifications").QueueUrls[0];
+
+            while(true)
+            {
+                var response = sqsClient.ReceiveMessage(queueUrl);
+                // Check our response
+                if (response.Messages.Count > 0)
+                {
+                    foreach (var message in response.Messages)
+                    {
+                        // Send an email
+                        var body = (JObject)JsonConvert.DeserializeObject(message.Body, typeof(JObject));
+                        MessageBox.Show(body["Message"].ToString());
+
+                        // Delete our message so that it doesn't get handled again
+                        sqsClient.DeleteMessage(queueUrl, message.ReceiptHandle);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No more SQS message in queue");
+                    break;
+                }
+            }
+        }
+    }
+}
